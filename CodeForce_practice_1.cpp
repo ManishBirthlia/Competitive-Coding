86998e5e979dc68273920c223ab3381565f3484c
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-11-23 14:22:37 +0530

    Add files via upload

---
+++ CodeForce_practice_1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k;
+	cin>>n>>k;
+	int a[n];
+	for(int i=0;i<n;i++){
+	    cin>>a[i];
+	}
+	int ans=0;
+	for(int i=0;i<n;i++){
+	    if(a[i]>=a[k-1] && a[i]>0) ans++;
+	}
+	cout<<ans<<endl;
+	return 0;
+}
