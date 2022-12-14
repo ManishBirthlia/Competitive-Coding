948f61ba3002de281f7c289418e58059143eb8fc
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-12-14 14:35:53 +0530

    Add files via upload

---
+++ ODDPAIRS (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<2*(n/2)*((n+1)/2)<<endl;
+    }
+	return 0;
+}
