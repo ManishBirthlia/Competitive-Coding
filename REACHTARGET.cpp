cde6828b6baf6dc223056f36825597a3022d1aaa
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-01-03 14:21:44 +0530

    Add files via upload

---
+++ REACHTARGET.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>y){
+	        cout<<x-y<<endl;
+	    }else{
+	        cout<<0<<endl;
+	    }
+	}
+	return 0;
+}
