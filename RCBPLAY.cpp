ac20734fda2458a13f670ca475513a65cb73e884
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-01-31 17:16:53 +0530

    Add files via upload

---
+++ RCBPLAY.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        if(x+2*z>=y) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
