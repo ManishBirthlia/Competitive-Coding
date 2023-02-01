c759f3e541985f0e04cbe7451d7dfebf4fb1280d
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-02-01 23:22:12 +0530

    Add files via upload

---
+++ APPLEORANGE.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,ans,i;
+        cin>>n>>m;
+        cout<<gcd(n,m)<<endl;
+    }
+}
