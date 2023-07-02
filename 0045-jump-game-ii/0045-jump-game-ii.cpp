e86e62e710f6e1a6ec51f15028c4b684e88b149f
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-02 23:52:47 +0530

    Time: 16 ms (79.56%), Space: 16.5 MB (65.97%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,13 +3,11 @@ public:
     int jump(vector<int>& nums) {
         int idx=0,ans=0,maxi=0;
         for(int i=0;i<size(nums);i++){
-            if(i<=idx){
-                maxi=max(maxi,i+nums[i]);
-            }else{
+            if(i>idx){
                 ans++;
                 idx=maxi;
-                maxi=max(maxi,i+nums[i]);
             }
+            maxi=max(maxi,i+nums[i]);
         }
         return ans;
     }
