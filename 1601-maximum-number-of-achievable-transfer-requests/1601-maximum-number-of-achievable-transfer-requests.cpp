13d68e062dc06c15daf2b271f107fb5f04b731b5
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-03 01:21:25 +0530

    Time: 2091 ms (5.56%), Space: 412 MB (5.56%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int bal[21] = {};
-    int maximumRequests(int n, vector<vector<int>>& requests, int i = 0) {
-        if (i == requests.size())
-            return count(begin(bal), begin(bal) + n, 0) == n ? 0 : INT_MIN;
-        --bal[requests[i][0]];
-        ++bal[requests[i][1]];
-        auto take = 1 + maximumRequests(n, requests, i + 1);
-        ++bal[requests[i][0]];
-        --bal[requests[i][1]];
-        return max(take, maximumRequests(n, requests, i + 1));
+    int dfs(int n,vector<vector<int>>& req,vector<int>v,int ans=0,int i=0){
+        if(i==size(req)){
+            for(int i=0;i<n;i++){
+                if(v[i]!=0) return 0;
+            }
+            return ans;
+        }
+        int a,b;
+        a=dfs(n,req,v,ans,i+1);
+        v[req[i][0]]--;
+        v[req[i][1]]++;
+        b=dfs(n,req,v,ans+1,i+1);
+        return max(a,b);
+    }
+    int maximumRequests(int n, vector<vector<int>>& req) {
+        vector<int>v(n,0);
+        return dfs(n,req,v);
     }
 };
