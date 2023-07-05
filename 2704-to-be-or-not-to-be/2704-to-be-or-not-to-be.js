ccf051fc1ecb3e917f5b2aac2b1b4978dee5ddd5
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-05 21:35:25 +0530

    Time: 66 ms (15.18%), Space: 41.8 MB (63.47%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+     return {
+    toBe: function (otherVal) {
+      if (val !== otherVal) {
+        throw new Error("Not Equal");
+      }
+      return true;
+    },
+    notToBe: function (otherVal) {
+      if (val === otherVal) {
+        throw new Error("Equal");
+      }
+      return true;
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
