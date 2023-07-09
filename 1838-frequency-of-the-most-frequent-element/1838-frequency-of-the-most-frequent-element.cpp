a71a90401bcbe6351a56d30a0ae6a81530cf65b0
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-09 14:22:20 +0530

    Time: 268 ms (43.58%), Space: 98.9 MB (64.24%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int find(vector<int>& nums, int K,int i,int j,int temp=0){
-        while(j>=0 && K && nums[i]-nums[j]<=K){
-            temp++;
-            K-=nums[i]-nums[j];
-            j--;
+   int maxFrequency(vector<int>& A, int k) {
+        sort(begin(A), end(A));
+        long i = 0, N = A.size(), ans = 1, sum = 0;
+        for (int j = 0; j < N; ++j) {
+            sum += A[j];
+            while ((j - i + 1) * A[j] - sum > k) sum -= A[i++];
+            ans = max(ans, j - i + 1);
         }
-        return temp;
-    }
-    int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int ans=1;
-        for(int i=1;i<size(nums);i++){
-            if(nums[i]!=nums[i-1]) ans=max(find(nums,k,i-1,i-1),ans);
-        }
-        ans=max(find(nums,k,size(nums)-1,size(nums)-1),ans);
         return ans;
     }
 };
