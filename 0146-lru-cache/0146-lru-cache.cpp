6998fb1d7af23ebd9092780018c2fca6c33cf72b
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-19 01:46:55 +0530

    Time: 439 ms (98.30%), Space: 160.4 MB (99.90%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,67 @@
+class LRUCache {
+public:
+    inline static int M[10001];
+    inline static int16_t L[10002][2];
+    int cap, size = 0;
+    const int NONE = 10001;
+    int head = NONE, tail = NONE;
+    
+    LRUCache(int capacity) : cap(capacity) {
+        memset(M, 0xff, sizeof M);
+    }
+    
+    inline void erase(int key) {
+        const int pre = L[key][0];
+        const int nxt = L[key][1];
+        L[pre][1] = nxt;
+        L[nxt][0] = pre;
+        if (head == key) head = nxt;
+        if (tail == key) tail = pre;
+    }
+    
+    inline void push_front(int key) {
+        L[key][0] = NONE;
+        L[key][1] = head;
+        L[head][0] = key;
+        head = key;
+        if (tail == NONE)
+            tail = key;
+    }
+    
+    inline int pop_back() {
+        int ret = tail;
+        tail = L[tail][0];
+        L[tail][1] = NONE;
+        if (tail == NONE)
+            head = NONE;
+        return ret;
+    }
+    
+    int get(int key) {
+        if (M[key] == -1) return -1;
+        erase(key);
+        push_front(key);
+        return M[key];
+    }
+    
+    void put(int key, int value) {
+        if (M[key] != -1) {
+            erase(key);
+        } else {
+            if (size == cap) {
+                int poped = pop_back();
+                M[poped] = -1;
+                size -= 1;
+            }
+            size += 1;
+        }
+        push_front(key);
+        M[key] = value;
+    }
+};
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
