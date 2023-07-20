5c9268e69b3119fed20158485b5ef4444cd19629
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-21 00:17:29 +0530

    Time: 11 ms (92.72%), Space: 17.7 MB (76.60%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,11 +4,14 @@ public:
         stack<int>st;
         vector<int>ans;
         for(int i=0; i<size(asteroids); i++){
-            if(asteroids[i] > 0 || st.empty()) st.push(asteroids[i]);
+            int x=asteroids[i];
+            if(st.empty() || x>0) st.push(x);
             else{
-                while(!st.empty() && abs(asteroids[i]) > st.top() && st.top() > 0) st.pop();
-                if(!st.empty() && abs(asteroids[i]) == st.top()) st.pop();
-                else if(st.empty() || st.top() < 0) st.push(asteroids[i]);
+                while(!st.empty() && abs(x)>abs(st.top()) && st.top()>0) st.pop();
+                if(st.empty() || st.top()<0) st.push(x);
+                else if(st.top()==-x){
+                    st.pop();
+                }
             }
         }
         while(!st.empty()){
