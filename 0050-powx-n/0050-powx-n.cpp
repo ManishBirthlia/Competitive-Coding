4d860d7b9f669b8730d33bcddab74af62bac15e4
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-07-30 10:36:40 +0530

    Time: 3 ms (39.23%), Space: 5.8 MB (90.74%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n){
-        if(x==1) return 1;
-        else if(x==-1 && n%2==0) return 1;
-        else if(x==-1 && n%2==1) return -1;
-        if(n==-2147483648) n=-2147483647;
-        if(n<0){
-            x=1/x;
-            n*=-1;
-        }
-        double ans=1;
-        while(n){
-            if(!ans) return ans;
-            ans*=x;
-            n--;
-        }
-        return ans;
+       if(n==0) return 1;
+        if(n==1) return x;
+        if(n<0 && n!=INT_MIN) return myPow(1/x,-n);
+        else if(n<0) return myPow(1/x,-(n+2));
+        if(n%2==0)
+            return myPow(x*x,n/2);
+        return x*myPow(x*x,n/2);
     }
 };
