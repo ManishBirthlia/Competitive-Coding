5ee67eec0893e09c00912efa045b73c2f102e79f
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-08-12 11:17:36 +0530

    Time: 103 ms (95.69%), Space: 128.3 MB (15.70%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int tg=0,tc=0,cg=0,ans=0;
-        for(int i=0;i<size(gas);i++){
-            tg+=gas[i];
-            tc+=cost[i];
+        int total_cost=0,total_gas=0,curr_gas=0,ans=INT_MAX;
+        for(int i=0;i<size(cost);i++){
+            total_cost+=cost[i];
+            total_gas+=gas[i];
         }
-        if(tc>tg) return -1;
-        for(int i=0;i<size(gas);i++){
-            if(cg<0){
-                cg=0;
-                ans=i;
+        if(total_cost>total_gas) return -1;
+        for(int i=0;i<size(cost);i++){
+            curr_gas+=gas[i]-cost[i];
+            if(curr_gas<0){
+                ans=i+1;
+                curr_gas=0;
+            }else{
+                ans=min(ans,i);
             }
-            cg+=(gas[i]-cost[i]);
         }
         return ans;
     }
-    // int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-    //     int total_cost=0,total_fuel=0,n=cost.size();
-    //     for(int i=0;i<n;i++){
-    //         total_cost+=cost[i];
-    //         total_fuel+=gas[i];
-    //     }
-    //     if(total_fuel<total_cost){
-    //         return -1;
-    //     }
-    //     int curr_fuel=0,start=0;
-    //     for(int i=0;i<n;i++){
-    //         if(curr_fuel<0){
-    //             start=i;
-    //             curr_fuel=0;
-    //         }
-    //         curr_fuel+=(gas[i]-cost[i]);
-    //     }
-    //     return start;
-    // }
 };
