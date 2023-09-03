b01df367bf57fd37628e3a7b0de07d9fe856b1a6
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-09-03 10:07:34 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (24.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int dfs(int m, int n,int i,int j){
-        if(i==m || j==n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=dfs(m,n,i+1,j)+dfs(m,n,i,j+1);
-    }
-    int uniquePaths(int m, int n) {
-        dp.assign(m,vector<int>(n,-1));
-        return dfs(m,n,0,0);
+    int uniquePaths(int m, int n,int i=0,int j=0){
+        if(i==0 && j==0) dp.assign(m,vector<int>(n,0));
+        if(i == m-1 && j == n-1) return 1;
+        else if(i==m || j==n) return 0;
+        if(dp[i][j]) return dp[i][j];
+        return dp[i][j]=uniquePaths(m,n,i+1,j) + uniquePaths(m,n,i,j+1);
     }
 };
