accc1ac47a80bdff86e0348f93035afe858f15a8
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-09-10 15:06:45 +0530

    Time: 375 ms (93.71%), Space: 164.2 MB (95.15%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,64 +1,55 @@
 class LRUCache {
 public:
-    inline static int M[10001];
-    inline static int16_t L[10002][2];
-    int cap, size = 0;
-    const int NONE = 10001;
-    int head = NONE, tail = NONE;
     
-    LRUCache(int capacity) : cap(capacity) {
-        memset(M, 0xff, sizeof M);
-    }
-    
-    inline void erase(int key) {
-        const int pre = L[key][0];
-        const int nxt = L[key][1];
-        L[pre][1] = nxt;
-        L[nxt][0] = pre;
-        if (head == key) head = nxt;
-        if (tail == key) tail = pre;
-    }
-    
-    inline void push_front(int key) {
-        L[key][0] = NONE;
-        L[key][1] = head;
-        L[head][0] = key;
-        head = key;
-        if (tail == NONE)
-            tail = key;
-    }
-    
-    inline int pop_back() {
-        int ret = tail;
-        tail = L[tail][0];
-        L[tail][1] = NONE;
-        if (tail == NONE)
-            head = NONE;
-        return ret;
-    }
-    
+    unordered_map <int, int> m, cnt;
+    queue <int> q;
+    int n;
+    LRUCache(int capacity) : n(capacity) {}
     int get(int key) {
-        if (M[key] == -1) return -1;
-        erase(key);
-        push_front(key);
-        return M[key];
+        if (cnt.find(key) == cnt.end()) 
+            return -1;
+        q.push(key);
+        cnt[key]++;
+        return m[key];
+    }
+    void put(int key, int value) {
+        q.push(key);
+        cnt[key]++;
+        m[key] = value;
+        while (cnt.size() > n) {
+            int cur = q.front(); q.pop();
+            if (cnt[cur]-- == 1) 
+                cnt.erase(cur);
+        }
     }
     
-    void put(int key, int value) {
-        if (M[key] != -1) {
-            erase(key);
-        } else {
-            if (size == cap) {
-                int poped = pop_back();
-                M[poped] = -1;
-                size -= 1;
-            }
-            size += 1;
-        }
-        push_front(key);
-        M[key] = value;
-    }
+    // list<int>v;
+    // map<int,int>mp;
+    // int cap;
+    // LRUCache(int capacity) {
+    //     mp.clear();
+    //     v.clear();
+    //     cap=capacity;
+    // }
+    // int get(int key) {
+    //     if(!mp.count(key)) return -1;
+    //     v.remove(key);
+    //     v.push_front(key);
+    //     return mp[key];
+    // }
+    // void put(int key, int value) {
+    //     if(mp.count(key)){
+    //         v.remove(key);
+    //     }else if(v.size()==cap){
+    //         int last=v.back();
+    //         v.pop_back();
+    //         mp.erase(last);
+    //     }
+    //     v.push_front(key);
+    //     mp[key]=value;
+    // }
 };
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
