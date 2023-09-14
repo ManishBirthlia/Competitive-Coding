e0bb3c29eba39809eb6f081f7a7654dd6687a1e2
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-09-15 03:41:37 +0530

    Time: 19 ms (71.95%), Space: 17.4 MB (11.26%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    map<string, multiset<string>> targets;
+    vector<string> route;
+    void visit(string airport) {
+        while (targets[airport].size()) {
+            string next = *targets[airport].begin();
+            targets[airport].erase(targets[airport].begin());
+            visit(next);
+        }
+        route.push_back(airport);
+    }
+    vector<string> findItinerary(vector<vector<string>> tickets) {
+        for (auto ticket : tickets) targets[ticket[0]].insert(ticket[1]);
+        visit("JFK");
+        return vector<string>(route.rbegin(), route.rend());
+    }
+};
