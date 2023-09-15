dcc3ed7c4a3332b59483454e49216bc1e736d7a8
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-09-15 15:27:58 +0530

    Time: 774 ms (35.39%), Space: 177.4 MB (17.86%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,9 +1,3 @@
-class cmp{
-    public:
-    bool operator()(vector<int>&a,vector<int>&b){
-        return a[2]>b[2];
-    }
-};
 class Solution {
 public:
     vector<int>set;
@@ -18,20 +12,20 @@ public:
         int n=size(p),ans=0,count=0;
         set.assign(n,0);
         makeSet(p);
-        priority_queue<vector<int>,vector<vector<int>>,cmp>pq;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int d=abs(p[i][0]-p[j][0])+abs(p[i][1]-p[j][1]);
-                pq.push({i,j,d});
+                pq.push({d,i,j});
             }
         }
         while(!pq.empty() && count<n-1){
             auto it=pq.top();
             pq.pop();
-            int r1=find(it[0]),r2=find(it[1]);
+            int r1=find(it[1]),r2=find(it[2]);
             if(r1!=r2){
                 set[r1]=r2;
-                ans+=it[2];
+                ans+=it[0];
                 count++;
             }
         }
