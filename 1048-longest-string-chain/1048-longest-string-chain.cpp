2cafb6dfb403a3e5ac652e9b3e7adcd33a037a6c
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-09-23 22:47:10 +0530

    Time: 149 ms (39.08%), Space: 13.7 MB (57.07%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,33 @@
+class cmp{
+    public:
+    bool operator()(string &a,string &b){
+        return size(a)<size(b);
+    } 
+};
+class Solution {
+public:
+    int longestStrChain(vector<string>& w){
+        int n=size(w),ans=1;
+        vector<int>dp(n,1);
+        sort(w.begin(),w.end(),cmp());
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(size(w[i])+1==size(w[j])){
+                    int count=0,a=0,b=0;
+                    while(a<size(w[i]) && b<size(w[j])){
+                        if(w[i][a]==w[j][b]) a++;
+                        else count++;
+                        b++;
+                    }
+                    count+=size(w[j])-b;
+                    if(count==1){
+                        // cout<<i<<' '<<j<<endl;
+                        dp[j]=max(dp[j],dp[i]+1);
+                        ans=max(ans,dp[j]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
