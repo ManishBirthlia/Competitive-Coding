97d6b80e13f170d6ffb5c6dfb3b1c547ff3cca08
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-11-06 19:38:14 +0530

    Time: 10 ms (23.45%), Space: 9.2 MB (58.18%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int u[1001];
+    int u[1001][2];
     int Unionfind(int i){
-        while(u[i]!=i) i=u[i];
+        while(u[i][0]!=i) i=u[i][0];
         return i;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        for(int i=0;i<1001;i++) u[i]=i;
+        for(int i=0;i<1001;i++) u[i][0]=i;
         for(auto it:edges){
             int f=Unionfind(it[0]),s=Unionfind(it[1]);
             if(f==s) return it;
             else{
-                u[s]=f;
-                u[f]=u[s];
+                if(u[s][1]>u[f][1]){
+                    u[f][0]=s;
+                    u[f][1]++;
+                }else{
+                    u[s][0]=f;
+                    u[s][1]++;
+                }
             }
         }
         return {-1,-1};
