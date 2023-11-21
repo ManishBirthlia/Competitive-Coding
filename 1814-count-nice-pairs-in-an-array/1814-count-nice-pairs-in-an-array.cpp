a881617009e03669d2bf3b7e5cfdf9b7f1ee80f4
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-11-22 00:11:44 +0530

    Time: 108 ms (34.13%), Space: 57.5 MB (21.63%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    int rev(int n){
-        string s = to_string(n);
-        reverse(s.begin(),s.end());
-        return stoi(s);
-    }
+    long long m=1e9+7;
     int countNicePairs(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i=0;i<nums.size(); i++){
-            nums[i] = nums[i] - rev(nums[i]);
-            mp[nums[i]]++;
+        unordered_map<long long,long long>mp;
+        long long ans=0;
+        for(auto it:nums){ 
+            string s=to_string(it);
+            reverse(s.begin(),s.end());
+            long long rev=stoi(s);
+            mp[it-rev]++;
         }
-        long long res = 0;
-        for(auto &x:mp){
-            res += 1LL * x.second * (x.second - 1) /2;
-            res %= 1000000007;
+        for(auto it:mp){
+            ans=(ans+(it.second)*(it.second-1)/2)%m;
         }
-        return res;
+        return ans;
     }
-    // int m=1e9+7;
-    // int reverse(int n,int ans=0){
-    //     while(n){
-    //         ans+=n%10;
-    //         n/=10;
-    //         if(n) ans*=10;
-    //     }
-    //     return ans;
-    // }
-    // int countNicePairs(vector<int>& nums) {
-    //     unordered_map<int,int>mp;
-    //     long long ans=0;
-    //     for(auto it:nums) mp[abs(it-reverse(it))]++;
-    //     for(auto it:mp){
-    //         long long t=it.second,x=t*(t-1)/2;
-    //         ans=(ans+x)%m;
-    //     }
-    //     return ans;
-    // }
 };
