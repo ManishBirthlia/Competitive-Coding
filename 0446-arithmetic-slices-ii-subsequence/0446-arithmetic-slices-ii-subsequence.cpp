0fdeb354b701db1492a83c5c020b14566cb1a23d
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-01-08 00:28:53 +0530

    Time: 527 ms (59.72%), Space: 155 MB (47.22%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int ans=0,n=size(nums);
+        if(n==1 || n==2) return 0;
+        vector<unordered_map<long long,int>>dp(n);
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>-1;j--){
+                long long a=nums[i];
+                long long b=nums[j];
+                if(dp[j].count(a-b)){
+                    ans+=dp[j][a-b];
+                    dp[i][a-b]+=dp[j][a-b]+1;
+                }else dp[i][a-b]++;
+            }
+        }
+        return ans;
+    }
+};
