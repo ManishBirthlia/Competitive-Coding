5fb6330bb9a51604e68bf4b184f8656533fcc69a
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-01-08 18:34:46 +0530

    Time: 119 ms (13.68%), Space: 65 MB (51.38%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        // ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         stack<TreeNode*>st;
         int ans=0;
         st.push(root);
