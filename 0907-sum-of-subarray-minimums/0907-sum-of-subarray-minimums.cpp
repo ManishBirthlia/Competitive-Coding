a0f4c90cdd4251c37a91a4d09139e6f51f0eb40d
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-01-20 22:43:18 +0530

    Time: 100 ms (19.62%), Space: 44.6 MB (14.96%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        const int modval = 1e9 + 7;
+        int n = arr.size(), ans = 0;
+        vector<int> stck;
+        vector<long long > res(n);
+        for (int i = 0; i < n; i++) {
+            while (not stck.empty() and arr[i] <= arr[stck.back()]) stck.pop_back();
+            res[i] = arr[i] * (i - ((stck.empty()) ? -1 : stck.back()));
+            if (not stck.empty()) res[i] += res[stck.back()];
+            res[i] %= modval;
+            stck.push_back(i);
+            cout << res[i] << "\n";
+        }
+        for (long long  i : res) ans = (ans + i) % modval;
+        return ans;
+    }
+};
