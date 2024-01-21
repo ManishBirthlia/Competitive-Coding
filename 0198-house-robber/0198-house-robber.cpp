830deed1857f7a1bcbf66c545d8908b1c5eaa717
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-01-21 18:30:36 +0530

    Time: 0 ms (100.00%), Space: 9 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int rob(vector<int>& nums){
-        if(size(nums)==1) return nums[0];
-        vector<int>dp(size(nums),0);
+    int rob(vector<int>& nums) {
+        int n=size(nums),dp[n];
         dp[0]=nums[0];
+        if(n==1) return nums[0];
         dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<size(nums);i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
         }
-        return dp[size(nums)-1];
+        return dp[n-1];
     }
 };
