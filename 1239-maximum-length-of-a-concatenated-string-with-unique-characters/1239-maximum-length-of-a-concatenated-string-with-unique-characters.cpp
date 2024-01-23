1874fc114e4677fbfa0b50386df6817b218ceb7a
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-01-24 00:17:18 +0530

    Time: 226 ms (22.96%), Space: 115.1 MB (20.27%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -11,15 +11,12 @@ public:
         for(auto it:s1) if(mp.count(it)) return false;
         return true;
     }
-    int dfs(vector<string>& arr,string s="",int i=0){
+    int maxLength(vector<string>& arr,string s="",int i=0) {
         if(i==size(arr)) return size(s);
         if(dp.count(i) && dp[i].count(s)) return dp[i][s];
         int a,b;
-        a=dfs(arr,s,i+1);
-        b=check(s,arr[i])?dfs(arr,s+arr[i],i+1):0;
-        return dp[i][s]=max(a,b);
-    }
-    int maxLength(vector<string>& arr) {
-       return dfs(arr); 
+        a=maxLength(arr,s,i+1);
+        b=check(s,arr[i])?maxLength(arr,s+arr[i],i+1):0;
+        return dp[i][s]=max(a,b); 
     }
 };
