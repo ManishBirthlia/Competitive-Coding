b8f227c17740f60a0d3317e312eae5c37e74b19a
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-04-30 22:34:24 +0530

    Time: 48 ms (82.63%), Space: 13.9 MB (100.00%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string& word) {
+        int n=word.size();
+        long long cnt=0;
+        uint16_t freq[1024]={0};
+        freq[0]=1;
+        uint16_t sum=0;
+        for(int c: word){
+            int index=c-'a';
+            sum^=(1<<index);
+            cnt+=freq[sum]++;
+            for(char i=0; i<10; i++){
+                cnt+=freq[sum^(1<<i)];
+            }
+        }
+        return cnt;
+    }
+};
