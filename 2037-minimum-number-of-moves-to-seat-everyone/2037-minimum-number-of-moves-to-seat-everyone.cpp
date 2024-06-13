ef9cf2b89d105b726af29077d1a8852c792823bb
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-06-13 16:38:09 +0530

    Time: 6 ms (62.74%), Space: 21.1 MB (17.35%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -6,11 +6,7 @@ public:
         sort(students.begin(),students.end());
         int count=0;
         for(int i=0;i<size(seats);i++){
-            if(seats[i]>students[i]){
-                count+=seats[i]-students[i];
-            }else{
-                count+=students[i]-seats[i];
-            }
+            count+=abs(seats[i]-students[i]);
         }
         return count;
     }
