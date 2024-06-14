c8cae4abb17379c922dcb0d2c65fd9c2494915a3
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-06-14 10:41:37 +0530

    Time: 120 ms (59.45%), Space: 69.1 MB (79.13%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        sort(nums.begin(),nums.end());
-        int ans=0,curr=nums[0];
-        for(auto it:nums) mp[it]++;
-        for(auto it:nums){
-            if(curr<it) curr=it;
-            while(mp[it]!=1 && mp.count(curr)){
-                curr++;
+        int count =0;
+         sort(nums.begin(),nums.end());
+         for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
+                int diff=1;
+                count+=diff;
+                nums[i]=nums[i]+diff;
             }
-            if(mp[it]!=1){
-                mp[curr]++;
-                ans+=curr-it;
+            if(nums[i]<nums[i-1]){
+                int diff=nums[i-1]-nums[i]+1;
+                count+=diff;
+                nums[i]=nums[i]+diff;
             }
-            mp[it]--;
-        }
-        return ans;
+         }
+         return count;
+        // unordered_map<int,int>mp;
+        // sort(nums.begin(),nums.end());
+        // int ans=0,curr=nums[0];
+        // for(auto it:nums) mp[it]++;
+        // for(auto it:nums){
+        //     if(curr<it) curr=it;
+        //     while(mp[it]!=1 && mp.count(curr)){
+        //         curr++;
+        //     }
+        //     if(mp[it]!=1){
+        //         mp[curr]++;
+        //         ans+=curr-it;
+        //     }
+        //     mp[it]--;
+        // }
+        // return ans;
     }
 };
