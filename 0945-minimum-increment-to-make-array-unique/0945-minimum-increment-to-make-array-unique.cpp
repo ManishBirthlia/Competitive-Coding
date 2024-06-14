536bcaf1ae6968c3e0292d79011bd8b3c8e85492
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-06-14 10:42:14 +0530

    Time: 245 ms (15.35%), Space: 110.4 MB (9.05%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        int count =0;
-         sort(nums.begin(),nums.end());
-         for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-                int diff=1;
-                count+=diff;
-                nums[i]=nums[i]+diff;
-            }
-            if(nums[i]<nums[i-1]){
-                int diff=nums[i-1]-nums[i]+1;
-                count+=diff;
-                nums[i]=nums[i]+diff;
-            }
-         }
-         return count;
-        // unordered_map<int,int>mp;
-        // sort(nums.begin(),nums.end());
-        // int ans=0,curr=nums[0];
-        // for(auto it:nums) mp[it]++;
-        // for(auto it:nums){
-        //     if(curr<it) curr=it;
-        //     while(mp[it]!=1 && mp.count(curr)){
-        //         curr++;
+        // int count =0;
+        //  sort(nums.begin(),nums.end());
+        //  for(int i=1;i<nums.size();i++){
+        //     if(nums[i]==nums[i-1]){
+        //         int diff=1;
+        //         count+=diff;
+        //         nums[i]=nums[i]+diff;
         //     }
-        //     if(mp[it]!=1){
-        //         mp[curr]++;
-        //         ans+=curr-it;
+        //     if(nums[i]<nums[i-1]){
+        //         int diff=nums[i-1]-nums[i]+1;
+        //         count+=diff;
+        //         nums[i]=nums[i]+diff;
         //     }
-        //     mp[it]--;
-        // }
-        // return ans;
+        //  }
+        //  return count;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        unordered_map<int,int>mp;
+        sort(nums.begin(),nums.end());
+        int ans=0,curr=nums[0];
+        for(auto it:nums) mp[it]++;
+        for(auto it:nums){
+            if(curr<it) curr=it;
+            while(mp[it]!=1 && mp.count(curr)){
+                curr++;
+            }
+            if(mp[it]!=1){
+                mp[curr]++;
+                ans+=curr-it;
+            }
+            mp[it]--;
+        }
+        return ans;
     }
 };
